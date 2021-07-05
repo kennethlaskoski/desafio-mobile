59d80ad9727b665f14766a51839fd994eb5f5fe8
Kenneth Laskoski <kennethlaskoski@gmail.com>
2021-07-05 17:07:03 -0300

    Created Xcode project

---
+++ Currency/Shared/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Shared
+//
+//  Created by Kenneth Laskoski on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
